c185553902895dcb17f927bc6a94f52de941dc36
abougeniere <abougeniere@yahoo.fr>
2015-04-02 12:29:44 +0200

    first

---
+++ VTswitch/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Shell("c:\VTtest\vt_reseau.exe")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Shell("c:\VT\vt_reseau.exe")
+    End Sub
+End Class
